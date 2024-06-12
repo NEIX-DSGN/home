86a17843de44a8205552925b8d2175ff918f9f67
NEIX-DSGN <neix.dsgn@gmail.com>
2024-06-12 14:35:13 +0200

    update

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
-const track = document.getElementById("image-track");
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))],
+      randomString = length => Array.from(Array(length)).map(randomChar).join("");
 
-const handleOnUp = () => {
-  track.dataset.mouseDownAt = "0";  
-  track.dataset.prevPercentage = track.dataset.percentage;
-}
+const card = document.querySelector(".card"),
+      letters = card.querySelector(".card-letters");
 
 const handleOnMove = e => {
-  if(track.dataset.mouseDownAt === "0") return;
-  
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-  
-  const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
-  track.dataset.percentage = nextPercentage;
-  
-  track.animate({
-    transform: `translate(${nextPercentage}%, -50%)`
-  }, { duration: 1200, fill: "forwards" });
-  
-  for(const image of track.getElementsByClassName("image")) {
-    image.animate({
-      objectPosition: `${100 + nextPercentage}% center`
-    }, { duration: 1200, fill: "forwards" });
-  }
+  const rect = card.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+  letters.style.setProperty("--x", `${x}px`);
+  letters.style.setProperty("--y", `${y}px`);
+
+  letters.innerText = randomString(1500);    
 }
 
-/* -- Had to add extra lines for touch events -- */
+card.onmousemove = e => handleOnMove(e);
 
-window.onmousedown = e => handleOnDown(e);
-
-window.ontouchstart = e => handleOnDown(e.touches[0]);
-
-window.onmouseup = e => handleOnUp(e);
-
-window.ontouchend = e => handleOnUp(e.touches[0]);
-
-window.onmousemove = e => handleOnMove(e);
-
-window.ontouchmove = e => handleOnMove(e.touches[0]);
+card.ontouchmove = e => handleOnMove(e.touches[0]);
