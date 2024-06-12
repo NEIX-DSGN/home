bc2d8d68bb5d94895b0ca3878a2593b1b4a27b01
NEIX-DSGN <neix.dsgn@gmail.com>
2024-06-12 18:14:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 36;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 62)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 32;
+  }, 8);
+}
